31d7316835d37771d4786601279e3b7a5e6394aa
MisterCron <komron-at@mail.ru>
2021-09-09 17:31:35 +0700

    new version

--- BanClientId.lua
+++ BanClientId.lua
@@ -1,13 +1,30 @@
-local name = {["5D0EC22B2E1D4A54865A2D9461A89E20"] = true}
+Cconfig = data.openConfig('.\\plugins\\BanClientId\\config.json', 'json', '{}')
+
+if Cconfig:get('banname') == nil then
+    Cconfig:set('banname', {["Alex"] = "5D0EC22B2E1D4A54865A2D9461A89E20"})
+end
+
+Banname = Cconfig:get('banname')
+
+function Reload()
+    Cconfig:reload()
+    Banname = Cconfig:get('banname')
+end
 
 mc.listen("onPreJoin", function(pl)
     local A = pl:getDevice()
     local E = A.clientId
-    if name[E] then
-        print('Ban "' .. pl.name .. '"')
-        pl:disconnect("§l§cВаше устройство забанено на сервере")
-        pl:kick("§l§cВаше устройство забанено на сервере")
+
+    if Banname[pl.name] == E then
+        colorLog('red', 'Ban "' .. pl.name .. '"')
+        pl:kick(
+            "\n\n§l§cВаше устройство забанено на сервере")
     end
+end)
+
+mc.regConsoleCmd("banreload", "banreload", function(pl)
+    Reload()
+    colorLog("green", "Banreload")
 end)
 
 print('[\27[92mCRON\27[0m] \27[93mBanClientId loaded\27[0m')
